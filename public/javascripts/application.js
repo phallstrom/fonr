4282ab559b5e1a1e083954a16d2de767b051d5ef
Philip Hallstrom <philip@pjkh.com>
2009-04-09 13:06:16 -0700

    basic layout and homepage

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function about() {
+  if (Element.empty('about-content')) {
+    new Ajax.Updater('about-content', 'rails/info/properties', {
+      method:     'get',
+      onFailure:  function() {Element.classNames('about-content').add('failure')},
+      onComplete: function() {new Effect.BlindDown('about-content', {duration: 0.25})}
+    });
+  } else {
+    new Effect[Element.visible('about-content') ? 
+      'BlindUp' : 'BlindDown']('about-content', {duration: 0.25});
+  }
+}
